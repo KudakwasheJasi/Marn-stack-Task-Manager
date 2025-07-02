6cb48ee58e9c94a266d22002e6edad2d238eef03
Kudakwashe Jasi <kudakwashejasi4@gmail.com>
2025-07-02 13:28:07 +0200

    Fix MongoDB connection by updating environment variable names

--- server/utils/index.js
+++ server/utils/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 
 export const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("DB connection established");
   } catch (error) {
