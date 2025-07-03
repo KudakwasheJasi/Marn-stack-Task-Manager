cef906b3bff7d80330f87a5578213aed5925ee4f
Kudakwashe Jasi <kudakwashejasi4@gmail.com>
2025-07-03 21:05:34 +0200

    Fix API URL configuration and import paths

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { API_URL } from '../config';
+import { API_URL } from '../../config';
 
-const API_URI = `${API_URL}/api`;
+const API_URI = `${API_URL}`;
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
 
