4a9c1435a5ca97d6d4f812f9cfbd61b47a7e0bd2
Kudakwashe Jasi <kudakwashejasi4@gmail.com>
2025-07-03 11:33:45 +0200

    Update API URL to use correct Render deployment URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://task-manager-api.onrender.com',  // Update to your deployed server URL
+    baseURL: 'https://marn-stack-task-manager.onrender.com/api',  // Update to your deployed server URL with /api prefix
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
